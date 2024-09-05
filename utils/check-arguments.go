9417545829713f2ed662bf176879e83556595148
Tchoupinax <corentinfiloche@hotmail.fr>
2024-09-05 15:06:25 +0200

    feat: improve regex to check label name

--- utils/check-arguments.go
+++ utils/check-arguments.go
@@ -6,11 +6,11 @@ import (
 )
 
 func CheckLabelKey(key string) error {
-	r, _ := regexp.Compile("^[a-z]{1}[a-z./]*[a-z]{1}$")
+	r, _ := regexp.Compile("^[a-z]{1}[a-z./-]*[a-z]{1}$")
 	if r.MatchString(key) {
 		return nil
 	} else {
-		return fmt.Errorf("The label key does not respect this regex: ^[a-z]{1}[a-z./]*[a-z]{1}$")
+		return fmt.Errorf("The label key does not respect this regex: ^[a-z]{1}[a-z./-]*[a-z]{1}$")
 	}
 }
 
